bbc495c5721e3359b7429c221b086eea034db12e
Franklin Collazo <franklinguitar2004@icloud.com>
2023-10-07 13:23:06 -0400

    Added ability to register events with intents, and on_ready() to indicate if the program is running

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,15 @@
+import discord
 
+#Intents allow the bot to retrieve certain events
+intent = discord.Intents.default()
+intent.message_content = True
+
+client = discord.client(intent=intent)
+
+
+bot_name = "BytePSU"
+
+#on_ready() begins when the program runs and outputs a statement letting the user know 
+@client.event
+async def on_ready():
+    print(f"{bot_name} is now running")
