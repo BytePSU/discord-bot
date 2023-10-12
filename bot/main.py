3990d32a9cb2e2bad9db58693199a023843a1af6
Adam Torres Encarnacion <admr0805@gmail.com>
2023-10-12 06:45:14 -0400

    Changed startup for class support

--- bot/main.py
+++ bot/main.py
@@ -2,22 +2,36 @@ import discord
 from discord import app_commands
 import random
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import json
-import requests
 
 
 load_dotenv()
-# Intents allow the bot to retrieve certain events
-intent = discord.Intents.default()
-intent.message_content = True
+TOKEN = os.getenv('BOT_TOKEN')
+GUILD_ID = os.getenv('BOT_GUILD_ID')
+MY_GUILD = MY_GUILD = discord.Object(id=GUILD_ID)
 
-client = discord.Client(intents=intent)
-tree = app_commands.CommandTree(client)
 
-key = os.getenv('BOT_TOKEN')
-guild_id = os.getenv('BOT_GUILD_ID')
-bot_name = 'BytePSU'
+class Bot(discord.Client):  
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        '''CommandTrees hold all the cmd states required to make slashes work.
+        Whenever you work with app cmds, the tree is used to store and work with them.''' 
+        self.tree = app_commands.CommandTree(self)
+    
+    async def setup_hook(self):
+        # This copies the global commands over to the guild/server.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+client = Bot()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is running!')
+    print('------')
 
 
 def check_for_key(internship, key):
@@ -30,32 +44,11 @@ def check_for_key(internship, key):
             return "https://cdn.discordapp.com/embed/avatars/0.png"
         return 'Unknown'
 
-
 with open('./database/internships.json') as f:
     internships_data = json.load(f)
 
-# Now you can use the `data` variable to access the contents of the JSON file
 
-# on_ready() begins when the program runs. It syncs the tree when called, and outputs a statement letting the user know the bot is ready.
-
-
-@client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id=guild_id))
-    print(f"{bot_name} is now running")
-
-
-@tree.command(name="we_are", description="Responds with the Penn State chant.", guild=discord.Object(id=guild_id))
-async def we_are(interact):
-    await interact.response.send_message('We are PENN STATE!')
-
-
-@tree.command(name="hello", description="Responds with a greeting message.", guild=discord.Object(id=guild_id))
-async def hello(interact):
-    await interact.response.send_message('Whats up!')
-
-
-@tree.command(name="get_internship", description="Grabs internship data of given index.", guild=discord.Object(id=guild_id))
+@client.tree.command(name="internships")
 async def get_internship(interact, index: int):
     with open('utils/embed_colors.txt', 'r') as f:
         random_colors = [int(line.strip(), 16) for line in f.readlines()]
@@ -65,31 +58,21 @@ async def get_internship(interact, index: int):
         await interact.response.send_message(f"Internship #{index} does not exist.")
         return
 
-    embed = discord.Embed(title=f"Internship #{index}",
-                          colour=discord.Colour(color),
-                          url=check_for_key(internships_data[index], 'link'))
-
+    embed = discord.Embed(
+        title=f"Internship #{index}", 
+        colour=discord.Colour(color), 
+        url=check_for_key(internships_data[index], 'link'))
     embed.set_thumbnail(url=check_for_key(internships_data[index], 'icon'))
-    embed.set_footer(
-        text="BytePSU", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-
-    embed.add_field(name="Company", value=check_for_key(
-        internships_data[index], 'company'))
-    embed.add_field(name="Job Title", value=check_for_key(
-        internships_data[index], 'title'))
-    embed.add_field(name="Required Education", value=check_for_key(
-        internships_data[index], 'educationLevel'))
-    embed.add_field(
-        name="Year", value=f"{check_for_key(internships_data[index],'season')} {check_for_key(internships_data[index],'yr')}")
-    embed.add_field(name="Location", value=check_for_key(
-        internships_data[index], 'loc'))
-    embed.add_field(
-        name="Salary", value=f"${check_for_key(internships_data[index],'monthlySalary')}/mo\n${check_for_key(internships_data[index],'hourlySalary')}/hr")
-    
-    embed.add_field(name="More Details", value=check_for_key(
-        internships_data[index], 'moreInfo'))
+    embed.set_footer(text="BytePSU", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.add_field(name="Company", value=check_for_key(internships_data[index], 'company'))
+    embed.add_field(name="Job Title", value=check_for_key(internships_data[index], 'title'))
+    embed.add_field(name="Required Education", value=check_for_key(internships_data[index], 'educationLevel'))
+    embed.add_field(name="Year", value=f"{check_for_key(internships_data[index],'season')} {check_for_key(internships_data[index],'yr')}")
+    embed.add_field(name="Location", value=check_for_key(internships_data[index], 'loc'))
+    embed.add_field(name="Salary", value=f"${check_for_key(internships_data[index],'monthlySalary')}/mo\n${check_for_key(internships_data[index],'hourlySalary')}/hr")
+    embed.add_field(name="More Details", value=check_for_key(internships_data[index], 'moreInfo'))
 
     await interact.response.send_message(embed=embed)
 
 
-client.run(key)
+client.run(TOKEN)
