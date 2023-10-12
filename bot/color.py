ed0bd24c32451dc8acdb166af987e1e9b8d5f034
ColdAndromeda <bensonzheng87901@gmail.com>
2023-10-12 17:09:23 -0400

    Added Error Handling

--- bot/color.py
+++ bot/color.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import requests
+import random
 
 def calc_avg_color(url):
 
@@ -34,5 +35,9 @@ def calc_avg_color(url):
 
         # Convert average RGB values to hexadecimal
         color_code = f"#{avg_red:02x}{avg_green:02x}{avg_blue:02x}"
-
-    return color_code
+        return color_code
+    else:
+        with open('utils/embed_colors.txt', 'r') as f:
+            random_colors = [int(line.strip(), 16) for line in f.readlines()]
+            color = random.choice(random_colors)
+        return color
