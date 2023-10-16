577e5f90a615c29ba851c73366cab2a710834418
Jason H <55156874+accountrev@users.noreply.github.com>
2023-10-16 17:37:25 -0400

    Code for review

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import traceback
 
 import json
 import discord
@@ -10,19 +11,14 @@ from utils import internship as its
 from utils.color import calc_avg_color
 
 
-
-its.update()
 load_dotenv()
-
-
 TOKEN = os.getenv('BOT_TOKEN')
 GUILD_ID = os.getenv('BOT_GUILD_ID')
 MY_GUILD = discord.Object(id=GUILD_ID)
 
 
-with open('database/internships.json', 'r') as f:
-    internships_data = json.load(f)
-    f.close()
+its.update_file()
+internships_data = its.open_file()
 
 
 
@@ -50,28 +46,67 @@ async def on_ready():
 
 @client.tree.command(name="internship")
 async def get_internship(interact, index: int):
+    try:
+        if index > len(internships_data) or index < 0:
+            await interact.response.send_message(f"Internship #{index} does not exist. Try again.")
+            return
 
-    if index > len(internships_data) or index < 0:
-        await interact.response.send_message(f"Internship #{index} does not exist. Try again.")
+        embed = discord.Embed(title=f"Internship #{index} - {its.check_for_key(internships_data[index], 'company')}",
+                            colour=discord.Colour(int(calc_avg_color(internships_data[index]['icon']).lstrip('#'), 16)),
+                            url=its.check_for_key(internships_data[index], 'link'))
+                            
+
+        embed.set_thumbnail(url=its.check_for_key(internships_data[index], 'icon'))
+        embed.add_field(name="Company", value=its.check_for_key(internships_data[index], 'company'))
+        embed.add_field(name="Job Title", value=its.check_for_key(internships_data[index], 'title'))
+        embed.add_field(name="Required Education", value=its.check_for_key(internships_data[index], 'educationLevel'))
+        embed.add_field(name="Year", value=f"{its.check_for_key(internships_data[index],'season')} {its.check_for_key(internships_data[index],'yr')}")
+        embed.add_field(name="Location", value=its.check_for_key(internships_data[index], 'loc'))
+        embed.add_field(name="Salary", value=f"${its.check_for_key(internships_data[index],'monthlySalary')}/mo\n${its.check_for_key(internships_data[index],'hourlySalary')}/hr")
+        
+        url_view = discord.ui.View() 
+        url_view.add_item(discord.ui.Button(label='Apply', style=discord.ButtonStyle.url, url=its.check_for_key(internships_data[index], 'link')))
+        url_view.add_item(discord.ui.Button(label='Test', style=discord.ButtonStyle.green))
+        await interact.response.send_message(embed=embed, view=url_view)
+    except Exception as e:
+        await interact.response.send_message(f"An exception has occured. Please refer to the traceback below and blame someone.\n```{traceback.format_exc()}```")
         return
-
-    embed = discord.Embed(title=f"Internship #{index}",
-                          colour=discord.Colour(int(calc_avg_color(internships_data[index]['icon']).lstrip('#'), 16)),
-                          url=its.check_for_key(internships_data[index], 'link'))
-                          
-
-    embed.set_thumbnail(url=its.check_for_key(internships_data[index], 'icon'))
-    embed.add_field(name="Company", value=its.check_for_key(internships_data[index], 'company'))
-    embed.add_field(name="Job Title", value=its.check_for_key(internships_data[index], 'title'))
-    embed.add_field(name="Required Education", value=its.check_for_key(internships_data[index], 'educationLevel'))
-    embed.add_field(name="Year", value=f"{its.check_for_key(internships_data[index],'season')} {its.check_for_key(internships_data[index],'yr')}")
-    embed.add_field(name="Location", value=its.check_for_key(internships_data[index], 'loc'))
-    embed.add_field(name="Salary", value=f"${its.check_for_key(internships_data[index],'monthlySalary'):.2f}/mo\n${its.check_for_key(internships_data[index],'hourlySalary'):.2f}/hr")
     
-    url_view = discord.ui.View() 
-    url_view.add_item(discord.ui.Button(label='Apply', style=discord.ButtonStyle.url, url=its.check_for_key(internships_data[index], 'link')))
-    url_view.add_item(discord.ui.Button(label='Test', style=discord.ButtonStyle.green))
-    await interact.response.send_message(embed=embed, view=url_view)
+
+@client.tree.command(name="update")
+async def update(interact):
+    global internships_data
+
+    try:
+        message = ""
+        changes = its.check_for_update()
+
+        if changes["changed"]:
+            its.update_file()
+            internships_data = its.open_file()
+
+            if changes["amount"] > 0:
+                message += f"Update! {changes['amount']} new internships have been added!"
+            elif changes["amount"] < 0:
+                message += f"Update! {changes['amount']} internships have been removed!"
+
+            message += f'\n\nDatabase updated! ({changes["old_amount"]} -> {len(internships_data)})'
+        
+            await interact.response.send_message(message)
+        else:
+            await interact.response.send_message(f"Database is up to date! ({len(internships_data)} internships)")
+
+    except Exception as e:
+        await interact.response.send_message(f"An exception has occured. Please refer to the traceback below and blame someone.\n```{traceback.format_exc()}```")
+        return
+    
+@client.tree.command(name="test")
+async def test(interact):
+    global internships_data
+
+    internships_data = its.open_file()
+
+    await interact.response.send_message(f"json file refreshed, {len(internships_data)}")
 
 
 
